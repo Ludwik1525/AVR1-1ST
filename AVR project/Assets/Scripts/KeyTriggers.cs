17a02e1e0454b0a40ed3ad7d258853edaead63d2
Ludwik1526 <mmichal1526@gmail.com>
2019-03-01 18:22:59 +0100

    Xylophone with sounds created

---
+++ AVR project/Assets/Scripts/KeyTriggers.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyTriggers : MonoBehaviour {
+
+
+    public GameObject key0lowest;
+    public GameObject key1;
+    public GameObject key2;
+    public GameObject key3;
+    public GameObject key4highest;
+    public AudioClip key0lowestClip;
+    public AudioClip key1Clip;
+    public AudioClip key2Clip;
+    public AudioClip key3Clip;
+    public AudioClip key4highestClip;
+    public AudioSource source;
+    
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col == key0lowest)
+        {
+           source.PlayOneShot(key0lowestClip);
+        }
+
+        if (col == key1)
+        {
+            source.PlayOneShot(key1Clip);
+        }
+
+        if (col == key2)
+        {
+            source.PlayOneShot(key2Clip);
+        }
+
+        if (col == key3)
+        {
+            source.PlayOneShot(key3Clip);
+        }
+
+        if (col == key4highest)
+        {
+            source.PlayOneShot(key4highestClip);
+        }
+    }
+}
---
+++ AVR project/Assets/Scripts/TemporaryTestKeys.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemporaryTestKeys : MonoBehaviour {
+
+    public GameObject key0lowest;
+    public GameObject key1;
+    public GameObject key2;
+    public GameObject key3;
+    public GameObject key4highest;
+    public AudioClip key0lowestClip;
+    public AudioClip key1Clip;
+    public AudioClip key2Clip;
+    public AudioClip key3Clip;
+    public AudioClip key4highestClip;
+    public AudioSource source;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            source.PlayOneShot(key0lowestClip);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            source.PlayOneShot(key1Clip);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            source.PlayOneShot(key2Clip);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            source.PlayOneShot(key3Clip);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            source.PlayOneShot(key4highestClip);
+        }
+    }
+}
