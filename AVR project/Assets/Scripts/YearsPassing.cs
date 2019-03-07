32d410874fb7ce6a48a4307ce80ca2cc6a48e419
Ludwik1526 <mmichal1526@gmail.com>
2019-03-07 21:31:41 +0100

    Planets speed dependent on sun mass + star sounds

--- AVR project/Assets/Scripts/YearsPassing.cs
+++ AVR project/Assets/Scripts/YearsPassing.cs
@@ -9,15 +9,37 @@ public class YearsPassing : MonoBehaviour
     private float speed;
     private PlanetSpawner planetSpawner;
     private float distanceToSun;
+    private float sunMass;
 
     void Start ()
     {
         planetSpawner = GetComponent<PlanetSpawner>();
         distanceToSun = Vector3.Distance(sunLocation.gameObject.transform.position, this.transform.position);
+        switch (GameObject.FindGameObjectWithTag("Star").name)
+        {
+            case "LowestToneStar":
+                sunMass = 2;
+                break;
+            case "LowToneStar":
+                sunMass = 4;
+                break;
+            case "MiddleToneStar":
+                sunMass = 6;
+                break;
+            case "BigToneStar":
+                sunMass = 10;
+                break;
+            case "HighToneStar":
+                sunMass = 14;
+                break;
+            default:
+                sunMass = 1;
+                break;
+        }
     }
 	
 	void Update () {
         transform.RotateAround(sunLocation.transform.position, sunLocation.transform.up, speed * Time.deltaTime);
-        speed = 150000/(mass * distanceToSun);
+        speed = sunMass*(15000/(mass * distanceToSun));
     }
 }
