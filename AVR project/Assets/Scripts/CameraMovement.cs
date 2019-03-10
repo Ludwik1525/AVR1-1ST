f4e26a4ed378cf0b9061319e719fec063364ffc3
Simon Bech Jensen <life.lime@gmail.com>
2019-03-10 19:01:34 +0100

    movement changed

--- AVR project/Assets/Scripts/CameraMovement.cs
+++ AVR project/Assets/Scripts/CameraMovement.cs
@@ -6,26 +6,30 @@ public class CameraMovement : MonoBehaviour {
 
 	public Transform anchor;
     public float speed;
+    public float maxDistancce;
+    public Transform center;
     
 	void Start () {
+
 	}
 	
-	void Update () {
-		if(OVRInput.GetDown(OVRInput.Button.DpadDown))
+	void Update ()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.DpadDown) && Vector3.Distance(transform.position + (-anchor.forward * speed), center.position) < maxDistancce)
 		{
-			transform.Translate(-anchor.forward * speed);
+			transform.Translate((-anchor.forward * speed) * Time.deltaTime);
 		}
-		else if (OVRInput.GetDown(OVRInput.Button.DpadUp))
+		else if (OVRInput.GetDown(OVRInput.Button.DpadUp) && Vector3.Distance(transform.position + (anchor.forward * speed), center.position) < maxDistancce)
 		{
-			transform.Translate(anchor.forward * speed);
+			transform.Translate((anchor.forward * speed) * Time.deltaTime);
 		}
-		else if (OVRInput.GetDown(OVRInput.Button.DpadLeft))
+		else if (OVRInput.GetDown(OVRInput.Button.DpadLeft) && Vector3.Distance(transform.position + (-anchor.right * speed), center.position) < maxDistancce)
 		{
-			transform.Translate(-anchor.right * speed);
+			transform.Translate((-anchor.right * speed) * Time.deltaTime);
 		}
-		else if (OVRInput.GetDown(OVRInput.Button.DpadRight))
+		else if (OVRInput.GetDown(OVRInput.Button.DpadRight) && Vector3.Distance(transform.position + (anchor.right * speed), center.position) < maxDistancce)
 		{
-			transform.Translate(anchor.right * speed);
+			transform.Translate((anchor.right * speed) * Time.deltaTime);
 		}
 	}
 }
